a8e5661d1daaceca2199ddb1054fc8add73743f5
artep1kgames <133581349+artep1kgames@users.noreply.github.com>
2025-06-20 00:23:05 +0300

    Correct backend app path in run_all.sh for Uvicorn server execution

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-(cd backend && python3 -m uvicorn backend.app.main:app --reload) &
+(cd backend && python3 -m uvicorn app.main:app --reload) &
 (cd frontend && python3 -m http.server 5000) &
 wait
