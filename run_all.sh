048424654d08ff0edc69b0d45705faea8fb69ceb
artep1kgames <133581349+artep1kgames@users.noreply.github.com>
2025-06-20 00:28:01 +0300

    Update backend app path in run_all.sh for Uvicorn server execution

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-(cd backend && python3 -m uvicorn app.main:app --reload) &
+(cd backend/app && python3 -m uvicorn main:app --reload) &
 (cd frontend && python3 -m http.server 5000) &
 wait
